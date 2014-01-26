35aa304ee3ef8ca849378e0542cea4fccdeea752
Josh Moody <jgmoody@gmail.com>
2014-01-26 00:55:59 -0600

    Initial integration with Plates

--- src/controllers/Base.php
+++ src/controllers/Base.php
@@ -4,9 +4,28 @@ namespace Toadsuck\Skeleton\Controllers;
 
 class Base
 {
+	public $plates = null;
+	public $template = null;
+	public $base_path = null;
+		
 	public function __construct()
 	{
+		// We need to know paths to our resources.
+		$this->base_path = dirname(__DIR__);
 		
+		// Set up our template engine.
+		$this->plates = new \League\Plates\Engine($this->resolvePath('views'));
+
+		// Load URI extension using global variable
+		$this->plates->loadExtension(new \League\Plates\Extension\URI($_SERVER['PATH_INFO']));
+
+		$this->assets_path = dirname($_SERVER['SCRIPT_FILENAME']) . '/';
+
+		// Load the asset extension.
+		$this->plates->loadExtension(new \League\Plates\Extension\Asset($this->assets_path, true));
+		
+		// Setup a template.
+		$this->template = new \League\Plates\Template($this->plates);
 	}
 	
 	public function httpError($code = '404', $message = null)
@@ -28,5 +47,15 @@ class Base
 	public function __call($method = null, $args = null)
 	{
 		$this->httpError('404', 'Method does not exist');
+	}
+	
+	protected function resolvePath($resource)
+	{
+		return $this->base_path . '/' . $resource;
+	}
+	
+	protected function view($view, $data = null)
+	{
+		return $this->template->render($view, $data);
 	}
 }
--- src/controllers/Home.php
+++ src/controllers/Home.php
@@ -6,12 +6,19 @@ class Home extends Base
 {
 	public function __construct()
 	{
+		parent::__construct();
+
+		// Tell the template engine to look in views/home for our templates.
+		$this->plates->addFolder('home', $this->resolvePath('views/home'));
 		
+		// Set our default template.
+		$this->template->layout('layouts/default');
 	}
 	
 	public function index($id = null)
 	{
-		printf('In %s method with id = %s', __METHOD__, $id);
+		echo $this->view('home::index');
+		
 	}
 	
 	public function foo($id = null)
