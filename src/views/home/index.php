6880db8d42ff5180a068f3fff402a588a72fe0b5
Josh Moody <jgmoody@gmail.com>
2014-01-27 22:31:04 -0600

    Cleanup

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -1,5 +1,3 @@
 <h1><?=$this->heading?></h1>
 
-<h2>Hello, <?=$this->name?>!!!</h2>
-
-<p>Want to see a <a href="<?=$this->uri('/home/captains')?>">list of Starfleet Captains</a>?</p>
+<h2>Hello, World!!!</h2>
