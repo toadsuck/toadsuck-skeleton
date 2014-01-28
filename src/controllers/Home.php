83f10b334bfd757411470de5c29d999f2d158d18
Josh Moody <jgmoody@gmail.com>
2014-01-27 22:30:15 -0600

    Removed example methods except for index. This is a skeleton, not a demo.

--- src/controllers/Home.php
+++ src/controllers/Home.php
@@ -23,49 +23,17 @@ class Home extends Controller
 		$this->config->load('config');
 		
 		// And our database config file.
-		$this->config->load('database');
+		#$this->config->load('database');
 		
 		// Initialize our database.
-		DB::init($this->config->get('dsn'));
+		#DB::init($this->config->get('dsn'));
 	}
 	
-	public function index($name = null)
+	public function index()
 	{
-		$this->template->name = !empty($name) ? $name : $this->config->get('default_name');
-		
 		$this->template->output('home/index', ['heading' => 'Congratulations, it worked!']);
 
 		// Instead of outputting the rendered content, you could return it:
 		//echo $this->template->render('home/index', ['heading' => 'Congratulations, it worked!']);
 	}
-	
-	public function captains()
-	{
-		$captains = Captain::all()->toArray();
-		$this->template->output('home/captains', ['captains' => $captains]);
-	}
-	
-	public function xss()
-	{
-		// We are protected against cross site scripting.
-		$this->template->name = "<script>alert('xss')</script>";
-		$array = [];
-		$array['foo'] = "<script>alert('foo')</script>";
-		$array['bar'] = "<script>alert('bar')</script>";
-		$this->template->array = $array;
-		$this->template->object = (object) $array; 
-		
-		$this->template->output('home/index', ['heading' => "XSS? Not on my watch."]);
-	}
-	
-	public function go()
-	{
-		$this->redirect('home/xss');
-	}
-
-	public function external()
-	{
-		$this->redirect('http://google.com');
-	}
-
 }
